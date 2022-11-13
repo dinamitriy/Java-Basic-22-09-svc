dc1da3231c7e9172eff7933631111b988a0b4316
{dinamitriy} <{dinamitriy@ya.ru}>
2022-11-13 11:54:51 +0300

    HomeWorkTestingSystem revision

--- HomeWorkTestingSystem.java
+++ HomeWorkTestingSystem.java
@@ -8,51 +8,36 @@ public class HomeWorkTestingSystem {
         questions[1] = "Как называется операция для выделения памяти под элементы массива?";
         questions[2] = "Какой применяется цикл, когда не известно количество итераций?";
         String[][] answerOptions = {
-                {"1. *.class", "2. *.java", "3. *.exe", "4. *.com"},
-                {"1. add", "2. clone", "3. new", "4. insert"},
-                {"1. while", "2. for", "3. loop"}
+                {"*.class", "*.java", "*.exe", "*.com"},
+                {"add",     "clone",  "new",   "insert"},
+                {"while",   "for",    "loop"}
         };
         int[] correctAnswers = new int[3];
         correctAnswers[0] = 1;
         correctAnswers[1] = 3;
         correctAnswers[2] = 1;
         Scanner scanner = new Scanner(System.in);
-        for(int i = 0; i < 1; i++) {
-            System.out.println(questions[0]);
-            System.out.println(answerOptions[0][0]);
-            System.out.println(answerOptions[0][1]);
-            System.out.println(answerOptions[0][2]);
-            System.out.println(answerOptions[0][3]);
-            System.out.print("Ваш ответ: ");
-            int answerForQuestion0 = scanner.nextInt();
-            if (answerForQuestion0 == correctAnswers[0]) {
-                System.out.println("Правильно");
-                correctCount ++;
-            } else {
-                System.out.println("Неправильно");
-                wrongCount ++;
+        for (int i = 0; i < questions.length; i++) {
+            System.out.println(questions[i]);
+            for (int j = 0; j < answerOptions[i].length; j++) {
+                System.out.println((j + 1) + ". " + answerOptions[i][j]);
             }
-            System.out.println(questions[1]);
-            System.out.println(answerOptions[1][0]);
-            System.out.println(answerOptions[1][1]);
-            System.out.println(answerOptions[1][2]);
-            System.out.println(answerOptions[1][3]);
             System.out.print("Ваш ответ: ");
-            int answerForQuestion1 = scanner.nextInt();
-            if (answerForQuestion1 == correctAnswers[1]) {
-                System.out.println("Правильно");
-                correctCount ++;
-            } else {
-                System.out.println("Неправильно");
-                wrongCount ++;
+            int answerForQuestion0 = 0;
+            while (scanner.hasNext()) {
+                if (scanner.hasNextInt()) {
+                    answerForQuestion0 = scanner.nextInt();
+                   if (answerForQuestion0 <= 0 || answerForQuestion0 > answerOptions[i].length) {
+                       System.out.println("Введено не число вне диапазона");
+                       System.out.print("Ваш ответ: ");
+                   } else {break;}
+                } else {
+                    System.out.println("Введено не число");
+                    System.out.print("Ваш ответ: ");
+                    scanner.next();
+                }
             }
-            System.out.println(questions[2]);
-            System.out.println(answerOptions[2][0]);
-            System.out.println(answerOptions[2][1]);
-            System.out.println(answerOptions[2][2]);
-            System.out.print("Ваш ответ: ");
-            int answerForQuestion2 = scanner.nextInt();
-            if (answerForQuestion2 == correctAnswers[2]) {
+            if (answerForQuestion0 == correctAnswers[i]) {
                 System.out.println("Правильно");
                 correctCount ++;
             } else {
